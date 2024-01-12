8b0d61bea31641b977b213e584a3f59d2e6b2a2d
andrii kalashnik <kalashnik.a.p@gmail.com>
2024-01-12 10:26:25 +0100

    updated styles, page, clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,50 +4,41 @@ import { AlignLeftOutlined, DownloadOutlined, FilterOutlined, MessageFilled, Sta
 import { Content, Header } from 'antd/es/layout/layout'; // TODO: Extract from 'antd'
 import BarChart from './components/BarChart';
 import DonutChart from './components/DonutChart';
-import styles from './page.module.css';
-
-// TODO: move styles to css file
-const headerStyle: React.CSSProperties = {
-  display: 'flex',
-  justifyItems: 'flex-start',
-  textAlign: 'center',
-  color: 'var(--primary-color)',
-  fontWeight: 'bold',
-  height: 64,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#dcdcdc',
-};
-
 
 export default async function Home() {
-
   const dateToFetch = '2022-01-11';
 
   async function getChartData() {
-    const jsonStructure = JSON.stringify({ date: "date", newCases: "newCasesByPublishDate", areaName: "areaName", femaleCases: "femaleCases" });
+    const jsonStructure = JSON.stringify({
+      date: 'date',
+      newCases: 'newCasesByPublishDate',
+      areaName: 'areaName',
+      newPeopleVaccinatedFirstDoseByPublishDate: 'newPeopleVaccinatedFirstDoseByPublishDate',
+      newPeopleVaccinatedSecondDoseByPublishDate: 'newPeopleVaccinatedSecondDoseByPublishDate'
+    });
+
     const apiUrl = `https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;date=${dateToFetch}&structure=${jsonStructure}`;
-    console.log(apiUrl);
+
     const res = await fetch(apiUrl);
 
     if (!res.ok) {
-      throw new Error('Failed to fetch data')
+      throw new Error('Failed to fetch data');
     }
 
-    return res.json()
+    return res.json();
   }
 
   const chartData = await getChartData()
 
   return (
-    <Layout className={styles.main}>
-      <Header style={headerStyle}>
+    <Layout>
+      <Header className='brandedHeader'>
         <div>MSD Test Assignment</div>
       </Header>
-      <Content style={{ padding: '0 48px' }}>
+      <Content className='brandedContent'>
         <Flex justify='space-between' align='center'>
-          <div>Statistics</div>
-          <Flex gap='middle' style={{ margin: '16px 0' }}>
+          <div>Dashboard</div>
+          <Flex gap='middle' style={{ margin: '1em 0' }}>
             <Button>
               <span>Export to PDF</span>
               <DownloadOutlined style={{ color: 'var(--primary-color)' }} />
@@ -59,6 +50,7 @@ export default async function Home() {
             </Button>
             <Button>
               <span>Filter</span>
+              <span className='filterIcon'>9</span>
               <FilterOutlined style={{ color: 'var(--primary-color)' }} />
             </Button>
           </Flex>
@@ -67,9 +59,9 @@ export default async function Home() {
           <Card
             // extra={<StarFilled />}  TODO: add favourite func
             style={{ width: '50%' }}
-            title='Bar Chart'
+            title={`Vaccination Status - ${dateToFetch}`}
             actions={[
-              <Flex justify='space-between'>
+              <Flex justify='space-between' className='sectionFooter'>
                 <Avatar src='https://xsgames.co/randomusers/avatar.php?g=pixel' />,
                 <MessageFilled />
               </Flex>,
@@ -80,7 +72,7 @@ export default async function Home() {
             style={{ width: '50%' }}
             title={`Cases by country - ${dateToFetch}`}
             actions={[
-              <Flex justify='space-between'>
+              <Flex justify='space-between' style={{ padding: '0 2em' }}>
                 <Avatar src='https://xsgames.co/randomusers/avatar.php?g=pixel' />,
                 <MessageFilled />
               </Flex>,
